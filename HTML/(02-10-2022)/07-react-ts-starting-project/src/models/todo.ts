88286bb329c90a228035908cc1b9b6c7ac1f6954
apelks <sharmakukan@gmail.com>
2022-02-10 20:46:51 +0530

    Pushing the work Update

---
+++ HTML/(02-10-2022)/07-react-ts-starting-project/src/models/todo.ts
@@ -0,0 +1,13 @@
+class todo {
+
+    id:string;
+    text:string;
+
+    constructor(todoText : string) {
+        this.text = todoText;
+        this.id = new Date().toISOString();
+    }
+
+}
+
+export default todo;
